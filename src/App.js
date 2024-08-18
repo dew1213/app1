9ff56498d4d704bc81a762d3d34799de88dec681
dew1213 <dew64527@gmail.com>
2024-08-18 15:28:29 +0700

    add components2, calculator, evet-data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
 import Calendar from "./class-components";
+import Button from "./class-components2";
 import { Header,Content,Footer } from "./func-components";
+import { Calculator } from "./calculator";
+import {EventData} from "./event-data";
 
  function App(){
     return(
       <>
-      <Header/>
-      <Content/>
-      <Footer/>
+      <Button/>
+      <Calculator/>
+      <EventData/>
+      
+      {/*  <Header/>
+       <Content/>
+       <Footer/> */}
       </>
     )
 }
